01f61579d25b3189780f4c4dbc0264a0e183bd64
Niraj Ramnani <161608562+Niraj-Ramnani@users.noreply.github.com>
2024-05-10 18:24:48 +0530

    Create program3-5.sh
    program 5 added

---
+++ program3-5.sh
@@ -0,0 +1,17 @@
+# script to check profit or loss 
+#!/bin/bash
+read -p "Enter selling price " sp
+read -p "Enter cost price " cp
+if [ $sp -gt $cp ]
+then
+ profit=$(( sp - cp ))
+        echo "There is  a profit of $profit "
+
+elif [ $cp -gt $sp ]
+then
+ loss=$(( cp - sp ))
+        echo "There is a loss of $loss "
+else
+        echo "There is no profit or loss "
+fi
+~ 
